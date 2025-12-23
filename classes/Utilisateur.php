c2cdc2dbd5349bc363b51f4191b40a316c136dd2
maryemlakhouil <lkhwilmaryem@gmail.com>
2025-12-23 16:49:28 +0100

    le fichier config/database.php

---
+++ classes/Utilisateur.php
@@ -0,0 +1,109 @@
+<?php
+
+require_once __DIR__ . '/../config/database.php';
+
+class Users{
+
+    protected int $id;
+    protected string $nom;
+    protected string $prenom;
+    protected string $email;
+    protected string $password;
+    protected string $role;
+    protected PDO $pdo;
+
+    // constructeur
+    public function __construct($id,$nom,$prenom,$email){
+        $pdo=Database::getConnection();
+        $this->id = id;
+        $this->email=email;
+        $this->prenom=prenom;
+        $this->nom=nom;
+    }
+   
+    // getters
+
+    public function getId(){
+        return $this->id;
+    }
+
+    public function getNom(){
+        return $this->nom;
+    }
+
+    public function getPrenom(){
+        return $this->prenom;
+    }
+
+    public function getEmail(){
+        return $this->email;
+    }
+
+    public function getRole(){
+        return $this->role;
+    }
+
+    // setters
+
+    public function setNom($nom){
+        $this->nom=$nom;
+    }
+
+    public function setPrenom($prenom){
+        $this->prenom=$prenom;
+    }
+
+    public function setEmail($email){
+        $this->email=$email;
+    }
+
+    public function setId($id){
+        $this->id=$id;
+    }
+
+    public function setRol($role){
+        $this->role=$role;
+    }
+
+    // Function Register
+    public function register(string $nom,string $prenom,string $email,string $password,string $role){
+        // Vérifier email déjà existant
+        $check = $this->pdo->prepare("SELECT id From users where email=? ");
+        $check ->execute([$email]);
+        
+        if ($check->rowCount() > 0) {
+            return false;
+        }
+        // hash du mot de passe 
+
+        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+        // Insertion
+        $sql = " INSERT INTO users (nom, prenom, email, password, role)
+            VALUES (?, ?, ?, ?, ?)
+        ";
+        $stmt = $this->pdo->prepare($sql);
+
+        return $stmt->execute([$nom,$prenom,$email,$hashedPassword,$role]);
+    }
+    // fonction de login    
+    public function login(string $email, string $password){
+
+        $sql = "SELECT * FROM users WHERE email = ?";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute([$email]);
+
+        $user = $stmt->fetch();
+
+        if (!$user) {
+            return false;
+        }
+
+        if (!password_verify($password, $user['password'])) {
+           return false;
+        }
+
+        return $user;
+    }
+}
+?>
--- config/database.php
+++ config/database.php
@@ -2,9 +2,8 @@
 /**
  * Connexion à la base de données (PDO)
  */
-
 class Database{
-    
+
     private static ?PDO $pdo = null;
 
     // Paramètres de connexion
@@ -15,7 +14,7 @@ class Database{
 
     /**
      * Retourne une instance PDO unique
-     */
+    */
     public static function getConnection(): PDO
     {
         if (self::$pdo === null) {
@@ -46,3 +45,41 @@ class Database{
     // Empêcher l'instanciation
     private function __construct() {}
 }
+Database::pdo
+
+
+// class Database
+// {
+//     private string $host = "localhost";
+//     private string $dbName = "coachconnect";
+//     private string $username = "root";
+//     private string $password = "abha11228899";
+
+//     private ?PDO $conn = null;
+
+//     public function connect(): PDO
+//     {
+//         if ($this->conn === null) {
+//             try {
+//                 $dsn = "mysql:host={$this->host};dbname={$this->dbName};charset=utf8mb4";
+
+//                 $this->conn = new PDO(
+//                     $dsn,
+//                     $this->username,
+//                     $this->password,
+//                     [
+//                         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+//                         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+//                         PDO::ATTR_EMULATE_PREPARES => false,
+//                     ]
+//                 );
+//             } catch (PDOException $e) {
+//                 die("Database connection failed: " . $e->getMessage());
+//             }
+//         }
+
+//         return $this->conn;
+//     }
+// }
+?>
+
