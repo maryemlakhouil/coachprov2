6a108bf522d01e1638e10129b2506fdbf9cfe0b8
maryemlakhouil <lkhwilmaryem@gmail.com>
2025-12-24 16:07:23 +0100

    new branch pour les chalenges

--- classes/Sportif.php
+++ classes/Sportif.php
@@ -38,6 +38,52 @@ class Sportif extends Utilisateur{
         return $stmt->fetchAll();
     }
 
+    // 3 - Reserver Une seance 
+   
+
+    // public function reserveSession(
+    //     int $sportifId,
+    //     int $coachId,
+    //     int $availabilityId
+    //  ): bool {
+
+    //     // Vérifier disponibilité
+    //     $check = $this->pdo->prepare(
+    //         "SELECT id FROM disponibilites
+    //          WHERE id = ? AND statut = 'libre'"
+    //     );
+    //     $check->execute([$availabilityId]);
+
+    //     if ($check->rowCount() === 0) {
+    //         return false;
+    //     }
+
+    //     // Créer réservation
+    //     $sql = "
+    //         INSERT INTO reservations
+    //         (sportif_id, coach_id, availability_id)
+    //         VALUES (?, ?, ?)
+    //     ";
+
+    //     $stmt = $this->pdo->prepare($sql);
+    //     $stmt->execute([
+    //         $sportifId,
+    //         $coachId,
+    //         $availabilityId
+    //     ]);
+
+    //     // Bloquer le créneau
+    //     $update = $this->pdo->prepare(
+    //         "UPDATE disponibilites
+    //          SET statut = 'reserve'
+    //          WHERE id = ?"
+    //     );
+    //     $update->execute([$availabilityId]);
+
+    //     return true;
+    // }
+
+
     
 
 
