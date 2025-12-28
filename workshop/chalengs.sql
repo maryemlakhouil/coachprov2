d2eae8b4f29b7f16249fc258a07d38ae6d3ed198
maryemlakhouil <lkhwilmaryem@gmail.com>
2025-12-28 17:33:40 +0100

    chalenge 7 done

--- workshop/chalengs.sql
+++ workshop/chalengs.sql
@@ -151,27 +151,127 @@ GROUP BY coach_id;
 
 -- afficher pour chaque coach taux de réservation (%)
 
-SELECT u.nom, u.prenom, (COUNT(s.id) / count(s.id) * 100) AS taux
+SELECT c.user_id, (COUNT(r.id) / count(s.id) * 100) AS taux_reservation
     FROM seances s
     JOIN coachs c ON s.coach_id = c.user_id
-    JOIN users u ON c.user_id = u.id
-    GROUP BY u.nom, u.prenom;
+    JOIN reservations r ON r.seance_id = s.id
+    GROUP BY c.user_id;
 
 -- afficher seulement les coachs ayant ≥3 séances
+select u.nom,u.prenom,count(s.id) as total_seances
+from  coachs c 
+join users u on u.id=c.user_id 
+join seances s on c.user_id = s.coach_id
+GROUP BY u.nom,u.prenom
+HAVING total_seances>=3;
 
- select  u.nom, u.prenom,count(s.id) as total_seances_Reservee
-    from seances s 
-    join reservations r on  s.id=seance_id
-    group by s.coach_id
-    having total_seances_Reservee>=3;
-    SELECT 
-    u.nom, u.prenom,
-    COUNT(s.id) AS total_seances,
-    COUNT(r.id) AS seances_reservees,
-    ROUND(COUNT(r.id)/COUNT(s.id)*100,2) AS taux_reservation
-FROM coachs c
-JOIN users u ON u.id = c.user_id
-JOIN seances s ON s.coach_id = c.user_id
-LEFT JOIN reservations r ON r.seance_id = s.id
-GROUP BY c.user_id
-HAVING COUNT(s.id) >= 3;
+/********************
+      chalenge 2 
+*********************/
+
+-- Lister les sportifs qui ont réservé le plus de séances par mois, avec :nom, prénom
+select u.nom,u.prenom,count(*) as total_reservations,DATE_FORMAT(r.reserved_at,'%M')as mois
+from reservations r 
+join users u on u.id= r.sportif_id
+group by u.id,mois
+order by total_reservations DESC;
+
+-- Lister les sportifs qui ont réservé le plus de séances par mois, avec nombre de réservations par mois
+SELECT date_format(r.reserved_at,'%M') as mois ,r.sportif_id ,count(*) as total_reservations
+from reservations r 
+join users u on u.id=r.sportif_id
+group by r.sportif_id,mois;
+
+-- mois et année et ordre décroissant par nombre de réservations
+select u.nom,u.prenom,count(*) as total_reservations,DATE_FORMAT(r.reserved_at,'%Y-%m')as année_mois
+from reservations r 
+join users u on u.id= r.sportif_id
+group by u.id,année_mois
+order by total_reservations DESC;
+
+/********************
+      chalenge 3
+*********************/
+
+-- Trouver les séances du même coach qui se chevauchent dans le temps :afficher coach, date, heure début, heure fin, id séance
+
+-- début A < fin B -- Et -- début B < fin A
+
+select u.nom as Nom_coach,u.prenom as Prenom_coach,s1.date_seance,s1.heure as Heure_Debut,DATE_ADD(s1.heure,INTERVAL s1.duree MINUTE) as heure_fin,s1.id as seance_id
+from seances s1 
+join seances s2 on s1.coach_id=s2.coach_id
+and s1.date_seance=s2.date_seance
+and s1.heure < DATE_ADD(s2.heure,INTERVAL s2.duree MINUTE)
+and s2.heure < DATE_ADD(s1.heure,INTERVAL s1.duree MINUTE)
+and  s1.id <>s2.id
+join users u on u.id=s1.coach_id;   
+
+/********************
+      chalenge 4
+*********************/
+-- Lister les coachs :qui n’ont aucune réservation depuis 60 jours
+
+select u.nom as nom_coach,u.prenom as Prenom_coach
+from coachs c 
+join users u on u.id=c.user_id
+left join seances s on c.user_id=s.coach_id
+left join  reservations r on s.id=r.seance_id
+group by u.nom,u.prenom
+having  max(r.reserved_at)is null or DATEDIFF(CURDATE(),MAX(r.reserved_at))>60;
+
+-- Lister les coachs :mais dont les sportifs ont réservé des séances récemment
+select u.nom as nom_coach,u.prenom as Prenom_coach
+from coachs c 
+join users u on u.id=c.user_id
+join seances s on c.user_id=s.coach_id
+join  reservations r on s.id=r.seance_id
+having  DATEDIFF(CURDATE(),r.reserved_at)<=60;
+
+/********************
+      chalenge 5
+*********************/
+
+-- Pour chaque discipline, afficher :les 3 coachs avec le plus de séances réservées inclure le nombre de réservations
+
+select c.discipline,u.nom as nom_coach,u.prenom as prenom_coach,count(r.id) as nb_reservations
+from coachs c 
+join users u on u.id = c.user_id
+join seances s on s.coach_id=c.user_id
+join reservations r on s.id=r.seance_id
+group by c.discipline,c.user_id
+order by nb_reservations desc ;
+
+
+/********************
+      chalenge 6
+*********************/
+
+-- Lister les sportifs : qui réservent toujours moins de 24h avant la séance
+
+select us.nom as nom_sportif,
+      us.prenom as prenom_sportif,
+      uc.nom as nom_coach,
+      us.prenom as prenom_coach,
+      count(r.id) as nb_reservations 
+from reservations r 
+join users us on r.sportif_id = us.id
+join seances s on s.id=r.seance_id
+join users uc on s.coach_id= uc.id
+group by r.sportif_id,s.coach_id
+having MAX( TIMESTAMPDIFF(HOUR,r.reserved_at,TIMESTAMP(s.date_seance,s.heure))) < 24;
+
+/********************
+      chalenge 7
+*********************/
+
+-- Trouver les plages horaires les plus réservées :regrouper par heure ou tranche 1h afficher nombre de séances réservées
+
+select FLOOR(HOUR(s.heure)/1) as tranche ,count(*) as seances_reservees
+from seances s 
+join reservations r on s.id = r.seance_id
+group by tranche
+order by seances_reservees desc;
+
+/********************
+      chalenge 8
+*********************/
